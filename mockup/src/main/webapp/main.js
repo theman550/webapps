90514e9177480edd72426b932306adf05b79dd47
Adrian Håkansson <adrian@adrianh.net>
2021-01-21 16:57:08 +0100

    add lab 2
    Co-authored-by: slnxc <slnxcx2@hotmail.com>
    Co-authored-by: Jacob Spilg <jacobspilg@hotmail.se>

---
+++ mockup/src/main/webapp/main.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    
+    $("#navbarNav").toggleClass("collapse");
+}
+
+function addEntry() {
+    var drinkName = $("#nameField").val();
+    $("#drinkList").append("<div class=\"col is-3\"><div class=\"card\"><img src=\"https://storhushall.skanemejerier.se/content/uploads/2019/05/Summer_Sunrise.jpg\" class=\"card-img-top\" alt=\"...\"><div class=\"card-body\"><h5 class=\"card-title\">"+drinkName+"</h5><p class=\"card-text\">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p></div></div></div>");
+}
