5a6234a17b2ddbbc27b80578f1b37ad47344a62e
andra <andra@IsakMagnusson>
2021-02-24 08:41:17 +0100

    fixed ingredient in DrinkListItem added CORS Filter

--- src/main/webapp/src/components/DrinkListItem.js
+++ src/main/webapp/src/components/DrinkListItem.js
@@ -6,12 +6,7 @@ import './DrinkListItem.css';
 export default function DrinkListItem(props) {
     const data = props.data;
     const tags = data.ingredients.map((ingredient) => 
-      <span key={ingredient} onClick={() => props.handleIngredientTagClick(ingredient)}>
-        <Chip 
-          className={`ingredient-tag pmr-2 p-mb-2 ${props.queries.includes(ingredient) ? "selected-ingredient" : ""}`} 
-          label={ingredient}>
-        </Chip>
-      </span>
+        <div className="ingredient-tag">{ingredient.name}</div>
     );
     return (
       <div className="p-col-12">
