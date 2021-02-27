75f7227f71555531c3a961768905cd29b2d38ce5
Oussama <Anadani>
2021-02-27 14:04:00 +0100

    changed drinkCard from a function to a class to be able to navigate to the details page when details-button clicked.

--- src/main/webapp/src/components/DrinkCard.js
+++ src/main/webapp/src/components/DrinkCard.js
@@ -2,45 +2,67 @@ import { Button } from "primereact/button";
 import { Avatar } from 'primereact/avatar';
 import { Chip } from 'primereact/chip';
 import Voter from './Voter.js';
+import ReactDOM from 'react-dom';
+import 'primeflex/primeflex.css';
+import { Switch, Route, withRouter }
+from 'react-router-dom';
+import React, { Component }
+from 'react';
 
+import Home from '../views/Home.js';
 import "./DrinkCard.css";
 
-export default function DrinkCard(props) {
-  const data = props.data;
+class DrinkCard extends Component {
 
-  const tags = data.ingredients.map((ingredient) => 
-    <span key={ingredient.id} onClick={() => props.handleIngredientTagClick(ingredient.name)}>
-      <Chip 
-        className={`ingredient-tag pmr-2 p-mb-2 ${props.queries.map((i) => i.name).includes(ingredient.name) ? "selected-ingredient" : ""}`} 
-        label={ingredient.name}>
-      </Chip>
-    </span>
-  );
-  return (
-    <div className="p-col-12 p-md-4 p-lg-3">
-      <div className="product-grid-item p-shadow-3">
-        <div className="product-grid-item-top">
-          <div>
-            <i className="pi pi-tag product-category-icon"></i>
-            {tags}
-          </div>
-          <Avatar icon="pi pi-user" shape="circle"></Avatar>
-        </div>
-        <div className="product-grid-item-content">
-          <Voter data={data} sendVote={props.sendVote}></Voter>
-          <img
-            src={data.image}
-            alt={data.name}
-          />
-          <div>
-            <div className="product-name">{data.name}</div>
-            <div className="product-description">{data.description}</div>
-          </div>
-        </div>
-        <div className="product-grid-item-bottom">
-          <Button label="Details" className="p-button-text"></Button>
-        </div>
-      </div>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+
+    }
+
+    navigate = (path) => {
+        this.props.history.push(path);
+    }
+
+    render() {
+        const tags = this.data.ingredients.map((ingredient) =>
+            <span key={ingredient.id} onClick={() => this.props.handleIngredientTagClick(ingredient.name)}>
+                <Chip 
+                    className={`ingredient-tag pmr-2 p-mb-2 ${this.props.queries.map((i) => i.name).includes(ingredient.name) ? "selected-ingredient" : ""}`} 
+                    label={ingredient.name}>
+                </Chip>
+            </span>
+        );
+
+        const data = this.props.data;
+        const detailsButton = <Button label="Login" onClick={() => this.navigate('/home')} ></Button>
+
+        return (
+                <div className="p-col-12 p-md-4 p-lg-3">
+                    <div className="product-grid-item p-shadow-3">
+                        <div className="product-grid-item-top">
+                            <div>
+                                <i className="pi pi-tag product-category-icon"></i>
+                                {tags}
+                            </div>
+                            <Avatar icon="pi pi-user" shape="circle"></Avatar>
+                        </div>
+                        <div className="product-grid-item-content">
+                            <Voter data={data} sendVote={this.props.sendVote}></Voter>
+                            <img
+                                src={data.image}
+                                alt={data.name}
+                                />
+                            <div>
+                                <div className="product-name">{data.name}</div>
+                                <div className="product-description">{data.description}</div>
+                            </div>
+                        </div>
+                        <div className="product-grid-item-bottom">
+                            <Button label="Details" className="p-button-text" onClick={() => this.navigate('/home')}>{detailsButton}</Button>
+                        </div>
+                    </div>
+                </div>
+                );
+    }
 }
+export default withRouter(DrinkCard);
