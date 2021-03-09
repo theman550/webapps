6c8f09536f396999411779485f6cfb3cb4deec24
Oussama <Anadani>
2021-03-09 20:02:19 +0100

    added detailsButton to the drinkListItem

--- src/main/webapp/src/components/DrinkListItem.js
+++ src/main/webapp/src/components/DrinkListItem.js
@@ -1,36 +1,43 @@
 import { Chip } from 'primereact/chip';
 import { Avatar } from 'primereact/avatar';
-
+import Details from '../views/Details.js';
 import Voter from './Voter.js';
 import './DrinkListItem.css';
 export default function DrinkListItem(props) {
     const data = props.data;
-    const tags = data.ingredients.map((ingredient) => 
-    <span key={ingredient.id} onClick={() => props.handleIngredientTagClick(ingredient.name)}>
-      <Chip 
-        className={`ingredient-tag pmr-2 p-mb-2 ${props.queries.map((i) => i.name).includes(ingredient.name) ? "selected-ingredient" : ""}`} 
-        label={ingredient.name}>
-      </Chip>
-    </span>
-  );
-    return (
-      <div className="p-col-12">
-        <div className="product-list-item">
-        <Voter data={data} sendVote={props.sendVote}></Voter>
-          <img
-            src={data.image}
-            alt={data.name}
-          />
-          <div className="product-list-detail">
-            <i className="pi pi-tag product-category-icon"></i>
-            {tags}
-            <div className="product-name">{data.name}</div>
-            <span className="product-description">{data.description}</span>
-          </div>
-          <div className="product-list-end">
-            <Avatar icon="pi pi-user" shape="circle"></Avatar>
-          </div>
-        </div>
-      </div>
+    const tags = data.ingredients.map((ingredient) =>
+        <span key={ingredient.id} onClick={() => props.handleIngredientTagClick(ingredient.name)}>
+            <Chip 
+                className={`ingredient-tag pmr-2 p-mb-2 ${props.queries.map((i) => i.name).includes(ingredient.name) ? "selected-ingredient" : ""}`} 
+                label={ingredient.name}>
+            </Chip>
+        </span>
     );
+    return (
+            <div className="p-col-12">
+                <div className="product-list-item">
+                    <Voter data={data} sendVote={props.sendVote}></Voter>
+                    <img
+                        src={data.image}
+                        alt={data.name}
+                        />
+                    <div className="product-list-detail">
+                        <i className="pi pi-tag product-category-icon"></i>
+                        {tags}
+                        <div className="product-name">{data.name}</div>
+                        <span className="product-description">{data.description}</span>
+                        <Details 
+                            src={data.image}
+                            alt={data.name}
+                            drinkName={data.name}
+                            ingredients= {tags}
+                            description={data.description}
+                            />
+                    </div>
+                    <div className="product-list-end">
+                        <Avatar icon="pi pi-user" shape="circle"></Avatar>
+                    </div>
+                </div>
+            </div>
+            );
 }
