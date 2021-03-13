dbfaba621cce1a62f6ba964513448b588ac72366
slnxc <slnxcx2@hotmail.com>
2021-03-13 23:38:55 +0100

    added endpoint for finding a single drink by ID

--- src/main/java/net/adrianh/drink/resources/DrinkResource.java
+++ src/main/java/net/adrianh/drink/resources/DrinkResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -190,5 +191,11 @@ public class DrinkResource {
         }
     }
     
-    
+    @GET
+    @Path("{id}")
+    @Consumes("*/*")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response drinkFromId(@PathParam("id") Long Id) throws JSONException {
+        return Response.status(Response.Status.OK).entity(drinkDAO.findDrinkByID(Id)).build();
+    }
 }
