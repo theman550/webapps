d5ee458f1141ad43a9e7916570047218741f4980
Oussama <Anadani>
2021-03-14 23:59:30 +0100

    added false some parameter in test files for getUpdatedUpvotes

--- src/test/java/net/adrianh/drink/model/dao/IngredientDAOTest.java
+++ src/test/java/net/adrianh/drink/model/dao/IngredientDAOTest.java
@@ -25,8 +25,6 @@ import net.adrianh.drink.model.entity.Vote;
  *
  * @author Jacob Spilg
  */
-
-
 @RunWith(Arquillian.class)
 public class IngredientDAOTest {
 
@@ -132,7 +130,7 @@ public class IngredientDAOTest {
     @Test
     //True if drinks[0] contains "d2", as it has a lower votecount than "d3" but same ingredient
     public void checkThatFindDrinksFromIngredientsMatchingNameFromOffsetWorks() {
-        List<Drink> drinks = ingredientDAO.findDrinksFromIngredientsMatchingNameFromOffset("Rum", 1, null).getResults();
+        List<Drink> drinks = ingredientDAO.findDrinksFromIngredientsMatchingNameFromOffset("Rum", 1, null, false).getResults();
         Assert.assertEquals("d2", drinks.get(0).getName());
     }
 
@@ -147,7 +145,7 @@ public class IngredientDAOTest {
                 return d2.getCreatedAt().compareTo(d1.getCreatedAt());
             }
         });
-        Assert.assertEquals(drinks.get(1).getName(), ingredientDAO.findDrinksFromIngredientsMatchingNameFromOffsetByNewest("Rum", 1, null).getResults().get(0).getName());
+        Assert.assertEquals(drinks.get(1).getName(), ingredientDAO.findDrinksFromIngredientsMatchingNameFromOffsetByNewest("Rum", 1, null, false).getResults().get(0).getName());
     }
 
     @After
